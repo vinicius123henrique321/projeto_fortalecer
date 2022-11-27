a60209e510ef33dab3cb3616d666af89ccc9a98a
Vinicius Henrique <112169674+vinicius123henrique321@users.noreply.github.com>
2022-11-27 20:51:45 -0300

    up

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/home")
+def home():
+  return render_template("home.html")
+
+@app.route("/sobrenós")
+def sobre():
+  return render_template("sobrenós.html")
+
+@app.route("/faleconosco")
+def conosco():
+  return render_template("faleconosco.html")
+
+
+
+
+
+
+if __name__ == '__main__':
+  app.run(debug=True)
